adef02b5eb555ec13c2713f6bab5ed087f3b99b8
Alison <147323252+Alison2014@users.noreply.github.com>
2025-01-11 21:08:47 -0300

    Update menu-mobile.js

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icone').src = "/img/menu-alt-svgrepo-com.svg"
+        document.querySelector('.icone').src = "img/menu-alt-svgrepo-com.svg"
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icone').src = "/img/close-lg-svgrepo-com.svg"
+        document.querySelector('.icone').src = "img/close-lg-svgrepo-com.svg"
     }
 }
