c79711cdf97860fb0468c517d403bb779e8f0a38
Alison <147323252+Alison2014@users.noreply.github.com>
2025-01-26 17:08:41 -0300

    Add files via upload

--- sites-desenvolvimento/SITE ARPLAN DEV/js/script.js
+++ sites-desenvolvimento/SITE ARPLAN DEV/js/script.js
@@ -13,4 +13,25 @@ function menuShow() {
     document.querySelector('#menu').classList.toggle('menu-aberto');
  }
 
+ /*CARROSSEL-OBRAS*/
+ $(document).ready(function(){
+    $('#owl-latest-project').owlCarousel({
+        items: 3, // Quantidade de itens por vez
+        loop: true, // Habilita o loop
+        margin: 10, // Margem entre os itens
+        autoplay: true, // Ativa o autoplay
+        autoplayTimeout: 3000, // Tempo de troca de imagem (em milissegundos)
+        responsive: {
+            0: {
+                items: 1 // Exibe 1 item em telas pequenas
+            },
+            600: {
+                items: 2 // Exibe 2 itens em telas médias
+            },
+            1000: {
+                items: 3 // Exibe 3 itens em telas grandes
+            }
+        }
+    });
+});
 
