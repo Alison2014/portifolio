ecb2423529f197daf2e46105787a5dc4d7628c86
Alison <147323252+Alison2014@users.noreply.github.com>
2025-06-27 12:18:53 -0300

    Add files via upload

--- sites-desenvolvimento/SITE ARPLAN DEV02/js/script.js
+++ sites-desenvolvimento/SITE ARPLAN DEV02/js/script.js
@@ -122,3 +122,17 @@ function menuShow() {
 
 
 
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Rolando para baixo: esconde o menu
+        header.style.top = "-110px"; // ajuste conforme a altura do seu header
+    } else {
+        // Rolando para cima: mostra o menu
+        header.style.top = "0";
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
