a2757b4f220c0330bb541e7d0537eeb8e08e549f
Alison <147323252+Alison2014@users.noreply.github.com>
2025-07-13 14:20:31 -0300

    Add files via upload

--- sites-desenvolvimento/SITE ARPLAN DEV02/js/script.js
+++ sites-desenvolvimento/SITE ARPLAN DEV02/js/script.js
@@ -54,26 +54,6 @@ function menuShow() {
 // FIM Função para alternar o menu mobile
 
 
-
-
-// Script para esconder o menu ao rolar a página
-let lastScrollTop = 0;
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', function () {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-        // Rolando para baixo: esconde o menu
-        header.style.top = "-110px"; // ajuste conforme a altura do seu header
-    } else {
-        // Rolando para cima: mostra o menu
-        header.style.top = "0";
-    }
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-});
-// FIM Script para esconder o menu ao rolar a página
-
-
 // Script para o slider de fotos na seção "Sobre" e "Arplan"
 let currentIndex = 0;
 const slider = document.querySelector('.foto_flex');
